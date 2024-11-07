bc3807ccb12bb2af8bbf42e0e7e537d439889034
BeamzXD <makes.k.s@mail.ru>
2024-11-07 14:12:31 +0300

    lab done

---
+++ StockApp/Program.cs
@@ -0,0 +1,185 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using System.IO;
+// using System.Net.Http;
+// using System.Text.Json;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using Newtonsoft.Json.Linq;
+// using Microsoft.Extensions.Configuration;
+
+// class StockPriceCalculator
+// {
+//     private static readonly HttpClient httpClient = new HttpClient();
+//     private static readonly object fileLock = new object();
+
+//     public static async Task Main()
+//     {
+//         var builder = new ConfigurationBuilder()
+//                     .AddUserSecrets<StockPriceCalculator>();
+//         IConfiguration configuration = builder.Build();
+
+//         string[] tickers = File.ReadAllLines("ticker.txt");
+
+//         List<Task> tasks = new List<Task>();
+//         foreach (string ticker in tickers)
+//         {
+//             tasks.Add(ProcessTickerAsync(ticker));
+//         }
+
+//         await Task.WhenAll(tasks);
+//         Console.WriteLine("Средние цены успешно записаны в файл.");
+//     }
+
+//     private static async Task ProcessTickerAsync(string ticker)
+//     {
+//         var builder = new ConfigurationBuilder()
+//                 .AddUserSecrets<StockPriceCalculator>();
+//         IConfiguration configuration = builder.Build();
+
+//         string ApiKey = configuration["AlphaVantageApiKey"];
+//         try
+//         {
+//             string url = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey={ApiKey}&outputsize=full";
+//             var response = await httpClient.GetStringAsync(url);
+
+//             var jsonData = JsonDocument.Parse(response);
+//             if (jsonData.RootElement.TryGetProperty("Time Series (Daily)", out var timeSeries))
+//             {
+//                 var prices = new List<double>();
+//                 foreach (var day in timeSeries.EnumerateObject())
+//                 {
+//                     var high = double.Parse(day.Value.GetProperty("2. high").GetString(), CultureInfo.InvariantCulture);
+//                     var low = double.Parse(day.Value.GetProperty("3. low").GetString(), CultureInfo.InvariantCulture);
+//                     prices.Add((high + low) / 2);
+//                 }
+
+//                 double averagePrice = prices.Average();
+//                 WriteToFile($"{ticker}: {averagePrice:F2}");
+//             }
+//             else
+//             {
+//                 Console.WriteLine($"Нет данных для тикера {ticker}.");
+//             }
+//         }
+//         catch (Exception ex)
+//         {
+//             Console.WriteLine($"Ошибка для тикера {ticker}: {ex.Message}");
+//         }
+//     }
+
+//     private static void WriteToFile(string line)
+//     {
+//         lock (fileLock)
+//         {
+//             File.AppendAllText("average_prices.txt", line + Environment.NewLine);
+//         }
+//     }
+// }
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+class lab091
+{
+    static readonly Mutex mutex = new Mutex();
+    static async Task Main()
+    {
+
+
+        List<string> tickers = new List<string>();
+
+        using (StreamReader reader = new StreamReader("ticker.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {//будет выполнять итерации до тех пор, пока ReadLine() не вернет null. Внутри цикла считываем каждую строку и добавляем ее в список tickers
+                tickers.Add(line);
+            }
+        }
+
+
+        List<Task> tasks = new List<Task>();
+        foreach (string ticker in tickers)
+        {
+
+            tasks.Add(GetDataForTicker(ticker));
+            System.Threading.Thread.Sleep(600);
+
+        }
+        await Task.WhenAll(tasks);
+
+
+    }
+
+    static async Task GetDataForTicker(string ticker)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+
+                string url = $"https://api.marketdata.app/v1/stocks/candles/D/{ticker}/?from=2023-11-08&to=2024-11-07&token=RFU4aDItQlRLRjFuNDd5OVlMWGs4UGh6eXY5bldMWDRvS0xxOHctcmNLOD0";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+                dynamic responceObject = Newtonsoft.Json.JsonConvert.DeserializeObject(responseContent);
+                double averagePrice = 0;
+                if (responceObject != null && responceObject.t != null && responceObject.h != null && responceObject.l != null)
+                {
+                    List<long> timestamps = responceObject?.t?.ToObject<List<long>>() ?? new List<long>();
+                    List<double> highs = responceObject?.h?.ToObject<List<double>>() ?? new List<double>();
+                    List<double> lows = responceObject?.l?.ToObject<List<double>>() ?? new List<double>();
+
+                    for (int i = 0; i < timestamps.Count; i++)
+                    {
+                        averagePrice += (highs[i] + lows[i]) / 2;
+                    }
+                    averagePrice /= timestamps.Count;
+                    if (averagePrice == 0)
+                    {
+                        Console.WriteLine($"Ошибка при обработке {ticker}: Отсутствуют данные.");
+                        await WriteToFile(ticker, averagePrice);
+                    }
+                    else
+                    {
+                        await WriteToFile(ticker, averagePrice);
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при обработке {ticker}: {ex.Message}");
+            }
+
+        }
+    }
+
+
+
+
+    private static async Task WriteToFile(string ticker, double averagePrice)
+    {
+        mutex.WaitOne();
+        try
+        {
+            using (StreamWriter writer = new StreamWriter("Average.txt", true))
+            {
+                await writer.WriteAsync($"{ticker}:{averagePrice} \n");
+                Console.WriteLine($"{ticker}: {averagePrice}");
+            }
+        }
+        finally
+        {
+            mutex.ReleaseMutex(); // Освобождаем мьютекс после завершения операции
+        }
+    }
+}
---
+++ WeatherApp/Program.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+
+
+class WeatherApp
+{
+    private static readonly HttpClient client = new HttpClient();
+    private static List<Grad> cities;
+
+    static async Task Main(string[] args)
+    {
+        // Load cities from file
+        cities = LoadCitiesFromFile("city.txt");
+
+        Console.WriteLine("Список городов:");
+        for (int i = 0; i < cities.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {cities[i].Name}");
+        }
+
+        Console.WriteLine("\nВведите номер города для получения прогноза погоды:");
+        if (int.TryParse(Console.ReadLine(), out int cityIndex) && cityIndex >= 1 && cityIndex <= cities.Count)
+        {
+            Grad selectedCity = cities[cityIndex - 1];
+
+            Weather? weather = await GetWeatherAsync(selectedCity.Lat, selectedCity.Lon);
+            if (weather != null)
+            {
+                Console.WriteLine(weather.Value.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Не удалось получить данные о погоде.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный выбор города.");
+        }
+    }
+
+    private static List<Grad> LoadCitiesFromFile(string filename)
+    {
+        List<Grad> cities = new List<Grad>();
+        foreach (string line in File.ReadAllLines(filename))
+        {
+            string[] parts = line.Replace(", ", "\t").Replace(".", ",").Split('\t');
+            if (parts.Length == 3)
+            {
+                Grad city = new Grad(
+                    parts[0].Trim(),
+                    Convert.ToDouble(parts[1]),
+                    Convert.ToDouble(parts[2])
+                );
+                cities.Add(city);
+            }
+        }
+        return cities;
+    }
+
+    private static async Task<Weather?> GetWeatherAsync(double lat, double lon)
+    {
+        try
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<WeatherApp>();
+            IConfiguration configuration = builder.Build();
+            string apiKey = configuration["OpenWeatherApiKey"];
+            var response = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={apiKey}");
+            dynamic json = JsonConvert.DeserializeObject(response);
+            if (json.sys.country != null)
+            {
+                return new Weather
+                {
+                    Country = json.sys.country,
+                    Name = json.name,
+                    Temp = json.main.temp,
+                    Description = json.weather[0].description
+                };
+            }
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при получении данных о погоде: {ex.Message}");
+            return null;
+        }
+    }
+}
+
+public struct Weather
+{
+    public string Country { get; set; }
+    public string Name { get; set; }
+    public float Temp { get; set; }
+    public string Description { get; set; }
+
+    public Weather(string country, string name, float temp, string description)
+    {
+        Country = country;
+        Name = name;
+        Temp = temp;
+        Description = description;
+    }
+
+    public override string ToString()
+    {
+        return $"Страна: {Country}\nНазвание города: {Name}\nТемпература воздуха: {Temp}°C\nОписание погоды: {Description}\n";
+    }
+}
+
+public class Grad
+{
+    public string Name { get; set; }
+    public double Lat { get; set; }
+    public double Lon { get; set; }
+
+    public Grad(string name, double lat, double lon)
+    {
+        Name = name;
+        Lat = lat;
+        Lon = lon;
+    }
+}
